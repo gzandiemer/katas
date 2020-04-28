94347191c543b730cb051c714e7ddf025117dab3
Günışığı Zan Diemer <43114601+gzandiemer@users.noreply.github.com>
2020-04-28 12:41:12 +0200

    create gzan/index.js

---
+++ 20200427-4/solutions/gzan/index.js
@@ -0,0 +1,17 @@
+function reverse(str){
+
+  //make arr of words
+  const arr = str.split(' ');
+  
+  //get rid of extra spaces
+  const cleaned = arr.filter(word => word.length != '');
+
+  //convert every other word to arr, reverse it and convert back to str
+  for(let i=1; i<cleaned.length; i+=2){
+    cleaned[i] = cleaned[i].split('').reverse().join('');
+  }
+  
+  //convert the new arr to str
+  const result = cleaned.join(' ');
+  return result;   
+}
